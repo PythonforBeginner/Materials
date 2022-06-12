026877024745d5600bb91171418e2ddabaddc1f2
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2022-06-12 13:09:20 -0700

    add folder

---
+++ Unit 3 Quizzes/To Lower Case/main.py
@@ -0,0 +1,10 @@
+def toLowerCase(s: str) -> str:
+    return s.lower()
+
+
+if __name__ == '__main__':
+    '''Test cases'''
+    print(toLowerCase("Hello"))
+    print(toLowerCase("hello"))
+    print(toLowerCase("HELLO"))
+
