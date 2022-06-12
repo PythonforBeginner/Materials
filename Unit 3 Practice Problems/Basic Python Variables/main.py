212eefacef43105cc6f4beb934aabccbcd5db290
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2022-06-12 12:53:59 -0700

    add solution

--- Unit 3 Practice Problems/Basic Python Variables/main.py
+++ Unit 3 Practice Problems/Basic Python Variables/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+def sum(num1: int, num2: int) -> int:
+    return num1 + num2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(sum(5, 6))
+    print(sum(1, 6))
+    print(sum(1, 1))
